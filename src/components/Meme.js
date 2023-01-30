10a1a9a9609befce667ce37319a3ba767de6b61f
Devansh Mishra <devanshunique@gmail.com>
2023-01-31 00:55:40 +0530

    Using React State to Change Image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,17 +1,29 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+    let [url, changeUrl] = React.useState("")
+
+    function handleClick() {
+        const memes = memesData.data.memes
+        const randomUrl = memes[Math.floor(Math.random()*memes.length)].url;
+        changeUrl(randomUrl)
+    }
+
     return (
         <div>
-            <form>
+            <div>
                 <div className="input--fields">
                     <input type="text" className="form--input" placeholder="Top Text"></input>
                     <input type="text" className="form--input" placeholder="Bottom Text"></input>
                 </div>
                 <div className="input--button">
-                    <button className="form--submit">Get a new meme image 🖼</button>
+                    <button onClick={handleClick} className="form--submit">Get a new meme image 🖼</button>
                 </div>
-            </form>
+            </div>
+            <div className="image--container">
+            <img src={url} alt="meme" className="meme--image"/>
+            </div>
         </div>
     )
 }
