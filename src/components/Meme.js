99f146be6870ad586b59879f4bcd89aeec051c83
Devansh Mishra <devanshunique@gmail.com>
2023-02-03 21:38:37 +0530

    Added Meme Object for React State

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,38 +2,31 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
-    let [memeImage, changeMemeImage] = React.useState("")
-    let [memeText, changeText] = React.useState({top: "", bottom: ""})
+    let [meme, updateMeme] = React.useState({topText: "", bottomText: ""})
+    let [allMemeImages, updateImages] = React.useState(memesData.data.memes)
 
     function getMemeImage() {
-        const memes = memesData.data.memes
-        const randomMeme = memes[Math.floor(Math.random()*memes.length)].url;
-        changeMemeImage(randomMeme)
-    }
-
-    function handleTop(event) {
-        changeText(prev=> [event.target.value, prev.bottom])
-    }
-
-    function handleBottom(event) {
-        changeText(prev=> [prev.top, event.target.value])
+        console.log(allMemeImages)
+        const image = allMemeImages[Math.floor(Math.random()*allMemeImages.length)].url;
+        console.log(image)
+        updateMeme(prevMem => {
+            return {...prevMem, randomImage: image};
+        })
     }
 
     return (
         <div>
-            <p>{memeText.top}</p>
-            <p>{memeText.bottom}</p>
             <div>
                 <div className="input--fields">
-                    <input type="text" className="form--input" value={memeText.top} onChange={handleTop} placeholder="Top Text"></input>
-                    <input type="text" className="form--input" value={memeText.bottom} onChange={handleBottom} placeholder="Bottom Text"></input>
+                    <input type="text" className="form--input" placeholder="Top Text"></input>
+                    <input type="text" className="form--input" placeholder="Bottom Text"></input>
                 </div>
                 <div className="input--button">
                     <button onClick={getMemeImage} className="form--submit">Get a new meme image 🖼</button>
                 </div>
             </div>
             <div className="image--container">
-                {memeImage && <img src={memeImage} alt="meme" className="meme--image"/>}
+                {meme.randomImage && <img src={meme.randomImage} alt="meme" className="meme--image"/>}
             </div>
         </div>
     )
