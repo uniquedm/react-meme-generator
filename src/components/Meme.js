85601fe911b5169c7311d4cff409fb8bb70c9240
Devansh Mishra <devanshunique@gmail.com>
2023-02-18 22:01:52 +0530

    Handle Submit

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -9,17 +9,8 @@ export default function Meme() {
         fetch('https://api.imgflip.com/get_memes')
     .then(response => response.json())
     .then(memes => updateImages(memes.data.memes))
-    console.log("Use Effect");
+    console.log("Fetched New Images!");
     }, [])
-
-    function getMemeImage() {
-        
-        const image = allMemeImages[Math.floor(Math.random()*allMemeImages.length)].url;
-        
-        updateMeme(prevMem => {
-            return {...prevMem, randomImage: image};
-        })
-    }
 
     function changeText(event) {
         const {name, value} = event.target
@@ -28,18 +19,25 @@ export default function Meme() {
         })
     }
 
+    function handleSubmit(event) {
+        event.preventDefault()
+        const image = allMemeImages[Math.floor(Math.random()*allMemeImages.length)].url;
+        updateMeme(prevMem => {
+            return {...prevMem, randomImage: image};
+        })
+    }
+
 
     return (
         <div>
+            <form onSubmit={handleSubmit}>
             <div>
                 <div className="input--fields">
-                    
                         <input id="top-input" type="text" onChange={changeText} value={meme.topText} name="topText" className="form--input" placeholder="Top Text"></input>
                         <input type="text" onChange={changeText} value={meme.bottomText} name="bottomText" className="form--input" placeholder="Bottom Text"></input>
-                   
                 </div>
                 <div className="input--button">
-                    <button onClick={getMemeImage} className="form--submit">Get a new meme image 🖼</button>
+                    <button className="form--submit">Get a new meme image 🖼</button>
                 </div>
             </div>
             {meme.randomImage && <div className="image--container">
@@ -50,6 +48,7 @@ export default function Meme() {
                     {meme.bottomText  && <p className="meme--text bottom">{meme.bottomText}</p>}
                 </fieldset>
             </div>}
+            </form>
         </div>
     )
 }
